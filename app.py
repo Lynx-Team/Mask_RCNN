9cbba864b7a9ea758a7fc20d6dd9a6ac8a4951f1
ptslog <hsfk@yandex.ru>
2019-01-18 11:47:55 +1000

    handle GET request on images/some_image.jpg

--- app.py
+++ app.py
@@ -15,56 +15,57 @@ app.config['OUTPUT_FOLDER'] = os.path.join(ROOT_DIR, 'output')
 model = CooksterNN(training=False, weights=Weights.LAST)
 
 def validate(input_file):
-    err_msg = ''
     if not input_file:
-        err_msg = 'File is not present in request'
+        raise ValueError('File is not present in request')
     elif input_file.filename == '':
-        err_msg = 'File name is not present in request'
+        raise ValueError('File name is not present in request')
     elif not input_file.filename.lower().endswith(('.jpg', '.jpeg', '.png')):
-        err_msg = 'Invalid filename: ' + input_file.filename
-    else:
-        return None
-
-    stderr.write(err_msg + '\n')
-    return BadRequest(err_msg)
+        raise ValueError('Invalid filename: ' + input_file.filename)
 
 def upload_file(request):
     if 'file' not in request.files:
-        stderr.write('Expected file in request.files\n')
-        return (None, BadRequest('Expected file in request'))
+        raise ValueError('Expected file in request.files')
 
     file = request.files.get('file')
-    bad_request = validate(file)
-    if bad_request is not None:
-        return (None, bad_request)
-
+    validate(file)
     path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
     file.save(path)
-    return (path, None)
+    return path
+
+def eval_and_save(request):
+    input_file = upload_file(request)
+    output_file = os.path.join(app.config['OUTPUT_FOLDER'], os.path.basename(input_file))
+    result = model.eval(input_file)
+    result.save(output_file)
+    return (result, output_file)
 
 @app.route('/products', methods=['POST'])
 def products_from_picture():
     try:
-        (input_file, bad_request) = upload_file(request)
-        if bad_request is not None:
-            return bad_request
-        else:
-            result = model.eval(input_file)
-            result.save(os.path.join(app.config['OUTPUT_FOLDER'], 'last_json_inference.jpg'))
-            return result.to_json()
+        (result, _) = eval_and_save(request)
+        return result.to_json()
     except Exception as e:
         stderr.write(str(e) + '\n')
         return BadRequest('Cannot recognize products')
 
 @app.route('/image', methods=['POST'])
-def image():
+def image_post():
     try:
-        (input_file, bad_request) = upload_file(request)
-        if bad_request is not None:
-            return bad_request
-        output_file = os.path.join(app.config['OUTPUT_FOLDER'], os.path.basename(input_file))
-        model.eval(input_file).save(output_file)
-        return send_file(output_file, mimetype='image/jpeg')
+        (_, output) = eval_and_save(request)
+        return send_file(output, mimetype='image/jpeg')
+    except Exception as e:
+        stderr.write(str(e) + '\n')
+        return BadRequest('Cannot recognize products')
+
+@app.route('/image/<string:filename>', methods=['GET'])
+def image_get(filename):
+    try:
+        filename = secure_filename(filename)
+        validate(filename)
+        file = os.path.join(app.config['OUTPUT_FOLDER'], filename)
+        if not os.path.isfile(file):
+            raise ValueError('Cannot find ' + file)
+        return send_file(file, mimetype='image/jpeg')
     except Exception as e:
         stderr.write(str(e) + '\n')
         return BadRequest('Cannot recognize products')
